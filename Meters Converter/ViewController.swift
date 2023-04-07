c407cd84975e5b7a74407e47eed488683ac72a94
Anastasio Almansa <at.almansa@gmail.com>
2023-04-07 10:50:28 +0100

    0.01.01 Commit Test: repository OK

--- Meters Converter/ViewController.swift
+++ Meters Converter/ViewController.swift
@@ -13,10 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        //hola caracola
-    print("Hola caracola")
+        //Listos para empezar
+        
+     
+        
     }
-
-
 }
 
