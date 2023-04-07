b18f1620f7397b4362a7496ce59821e59015344a
Anastasio Almansa <at.almansa@gmail.com>
2023-04-07 13:13:50 +0100

    0.0.0.2 - Calculations OK

--- Meters Converter/ViewController.swift
+++ Meters Converter/ViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let mile = 1.60934
+    //let kiloMeter = 0.1069
+    
+    
 
     @IBOutlet var distanceTextField: UITextField!
     
@@ -32,6 +37,32 @@ class ViewController: UIViewController {
     
     
     @IBAction func buttonConvert(_ sender: Any) {
+        
+        let unitSystem = segmentedControl.selectedSegmentIndex
+        
+        if let textField = Double(distanceTextField.text!)
+        {
+            // print("Selected: \(segmentedControl)")
+            //0 = meters; 1 = miles
+            
+            if unitSystem == 0 {
+                //lets convert to meters
+                let convertedValue = textField * mile
+                
+                
+                
+                labelResult.text = String(format:"%.2f",convertedValue) + " Km"
+                
+            }
+            else
+            {
+                //lets convert to miles
+                let convertedValue = textField / mile
+                labelResult.text = String(format:"%.2f",convertedValue) + " mi"
+            }
+            
+            
+        }
     }
     
 }
